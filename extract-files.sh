32421687fc849868763cdc1bd40f06065e7ee01c
Salvatore De Simone <salvatoredesim@gmail.com>
2016-03-08 11:26:40 +0100

    Modified extract-files.sh paths

--- extract-files.sh
+++ extract-files.sh
@@ -7,8 +7,8 @@ FP=$(cd ${0%/*} && pwd -P)
 export VENDOR=$(basename $(dirname $FP))
 export DEVICE=$(basename $FP)
 export BOARDCONFIGVENDOR=false
-export BOARD_VENDOR_PLATFORM=kanuti
+export BOARD_VENDOR_PLATFORM=kanutis
 export TARGET_BOARD_PLATFORM=msm8916
 
-../common/extract-files.sh $@
-../common/setup-makefiles.sh
+../sony_common/extract-files.sh $@
+../sony_common/setup-makefiles.sh
